dc2c05b29a06f6b175ccd0c3c305f701d0896027
danw5050 <danw5050@gmail.com>
2016-12-28 14:43:14 -0500

    added slider view for images

---
+++ Gallery/js/demo/demo.js
@@ -0,0 +1,51 @@
+/*
+ * blueimp Gallery Demo JS
+ * https://github.com/blueimp/Gallery
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+/* global blueimp, $ */
+$(function() {
+    'use strict'
+  
+            // Initialize the Gallery as image carousel:
+        blueimp.Gallery([{
+                title: 'Banana',
+                href: 'http://danielw.me/img/tweetmap.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'http://danielw.me/img/tweetmap.jpg'
+            },
+            {
+                title: 'Apple',
+                href: 'http://danielw.me/img/tweetmap.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'http://danielw.me/img/tweetmap.jpg'
+            }
+        ], {
+            container: '#blueimp-image-carousel',
+            carousel: true
+        })
+		
+		 blueimp.Gallery([{
+                title: 'Banana',
+                href: 'http://danielw.me/img/thephonelady.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'http://danielw.me/img/thephonelady.jpg'
+            },
+            {
+                title: 'Apple',
+                href: 'http://danielw.me/img/thephonelady.jpg',
+                type: 'image/jpeg',
+                thumbnail: 'http://danielw.me/img/thephonelady.jpg'
+            }
+        ], {
+            container: '#blueimp-image-carousel2',
+            carousel: true
+        })
+    
+
+})
